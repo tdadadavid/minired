297e857bb79b28d3cd9e922e8ca1d1f6bb7773b8
tdadadavid <davidtofunmidada@gmail.com>
2024-03-07 04:16:17 +0100

    writing and testing a simple resp parser

--- main.go
+++ main.go
@@ -1,51 +1,49 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net"
-	"os"
-)
+// import (
+// 	"fmt"
+// 	"io"
+// 	"net"
+// 	"os"
+// )
 
-func main() {
-	fmt.Println("Server listening on port 6379")
+// func main() {
+// 	fmt.Println("Server listening on port 6379")
 
 
-	//create a tcp listener on port 6379
-	listener, err := net.Listen("tcp", "6379")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+// 	//create a tcp listener on port 6379
+// 	listener, err := net.Listen("tcp", ":6379")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
 
-	//use listener to accepts any requests from client.
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+// 	//use listener to accepts any requests from client.
+// 	conn, err := listener.Accept()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
 
-	// close the connection once done with the requests
-	// [remember to study tcp,http protocols]
-	defer conn.Close() 
+// 	// close the connection once done with the requests
+// 	// [remember to study tcp,http protocols]
+// 	defer conn.Close() 
 
-	for {
-		// create a buffer in memory
-		buff := make([]byte, 1024)
+// 	for {
+// 		// create a buffer in memory
+// 		buff := make([]byte, 1024)
 
-		// read but ignore the content from the client
-		_, err := conn.Read(buff)
-		if err != nil { 
-			if err == io.EOF { //if there is end-of-file error break from the loop
-				break
-			}
-			fmt.Println("Error reading from client", err.Error())
-			os.Exit(1) // stop the server.
-		}
+// 		// read but ignore the content from the client
+// 		_, err := conn.Read(buff)
+// 		if err != nil { 
+// 			if err == io.EOF { //if there is end-of-file error break from the loop
+// 				break
+// 			}
+// 			fmt.Println("Error reading from client", err.Error())
+// 			os.Exit(1) // stop the server.
+// 		}
 
-		// for any request the user makes return a pong.
-		conn.Write([]byte("+Ok\r\n"))
-	}
-
-	fmt.Println("Hello world.")
-}
+// 		// for any request the user makes return a pong.
+// 		conn.Write([]byte("+PONG\r\n"))
+// 	}
+// }
---
+++ resp.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Reference study: https://redis.io/docs/reference/protocol-spec/
+
+// this is a simple RESP parser.
+func main() {
+	// given a string like this '$5/r/n/ahmed/r/n'
+	// we need to parse this, this string according to RESP
+	// is a bulk string, (a bulk string starts with $ and the length of the value (5))
+
+	input := "$5\r\nahmed\r\n"
+	reader := bufio.NewReader(strings.NewReader(input))
+
+	// read first byte from the buffer
+	firstCharBuf, _ := reader.ReadByte()
+
+	// now if the first byte read is not '$' then we reject it
+	if firstCharBuf != '$' {
+		fmt.Println("Error: Invalid type provided. Only bulk strings are supported")
+		os.Exit(1)
+	}
+
+	size, _ := reader.ReadByte()
+
+	inputSize, _ := strconv.ParseInt(string(size), 10, 64)
+
+	// current position in parsing: ['$', '5', '/r', '/n', 'ahmed', '/r', '/n']
+	//																			    ^
+	reader.ReadByte()
+
+	// current position in parsing: ['$', '5', '/r', '/n', 'ahmed', '/r', '/n']
+	//																						     ^
+	reader.ReadByte()
+
+
+	name := make([]byte, inputSize)
+	reader.Read(name)
+
+	fmt.Println(string(name))
+
+}
