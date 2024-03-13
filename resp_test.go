83328bb553c3679e18dfc154fcdc342f8d61758c
tdadadavid <davidtofunmidada@gmail.com>
2024-03-13 13:12:33 +0100

    remove test case

--- resp_test.go
+++ resp_test.go
@@ -17,14 +17,3 @@ func TestRESPParser(t *testing.T) {
 	})
 
 }
-
-func TestRESPParserFail(t *testing.T) {
-	// bad scenerio
-	t.Run("It stops the program when a non-bulk string command is sent", func(t *testing.T) {
-		request := "#6\r\nsixtyo\r\n"
-		result := ParseResp(request)
-
-		assert.Contains(t, result, "sixtyo")
-	})
-	//TODO: check if its is possible how to handle errors not returned.
-}
