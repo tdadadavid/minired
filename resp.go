bc37dd926385034c2c8c013c2ff1769ecd39df28
tdadadavid <davidtofunmidada@gmail.com>
2024-03-13 19:12:15 +0100

    refactor variable names

--- resp.go
+++ resp.go
@@ -4,9 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"os"
 	"strconv"
-	"strings"
 )
 
 // Reference study: https://redis.io/docs/reference/protocol-spec/
@@ -31,17 +29,17 @@ type Value struct {
 }
 
 
-type RESP struct {
+type Resp struct {
 	reader *bufio.Reader
 }
 
-func NewRESP(reader io.Reader) *RESP {
-	return &RESP{ reader: bufio.NewReader(reader) }
+func NewResp(reader io.Reader) *Resp {
+	return &Resp{ reader: bufio.NewReader(reader) }
 }
 
-func (resp *RESP) readLine() (line []byte, n int, err error) {
+func (r *Resp) readLine() (line []byte, n int, err error) {
 	for {
-		byte, err := resp.reader.ReadByte()
+		byte, err := r.reader.ReadByte()
 		if err != nil {
 			return nil, 0, err
 		}
@@ -62,8 +60,8 @@ func (resp *RESP) readLine() (line []byte, n int, err error) {
 }
 
 
-func (resp *RESP) readInteger() (num int, n int, err error) {
-	line, n, err := resp.readLine()
+func (r *Resp) readInteger() (num int, n int, err error) {
+	line, n, err := r.readLine()
 	if err != nil {
 		return 0, 0, nil
 	}
@@ -77,17 +75,17 @@ func (resp *RESP) readInteger() (num int, n int, err error) {
 	return int(_64bitInteger), n, nil
 }
 
-func (resp *RESP) Read() (Value, error) {
-	resp_type, err := resp.reader.ReadByte()
+func (r *Resp) Read() (Value, error) {
+	resp_type, err := r.reader.ReadByte()
 	if err != nil {
 		return Value{}, err
 	}
 
 	switch resp_type {
 	case ARRAY:
-		return resp.readArray()
+		return r.readArray()
 	case BULK:
-		return resp.readBulk()
+		return r.readBulk()
 	default:
 		fmt.Println("Unknown type: ", string(resp_type))
 		return Value{}, nil
@@ -95,7 +93,7 @@ func (resp *RESP) Read() (Value, error) {
 }
 
 
-func (resp *RESP) readArray() (Value, error) {
+func (r *Resp) readArray() (Value, error) {
 	value := Value{}
 	value.typ = "array"
 
@@ -103,7 +101,7 @@ func (resp *RESP) readArray() (Value, error) {
 	// ["*", "2", ""]
 	//        ^
 	// the 2 specifies the number of elements in the array request.
-	arr_len, _, err := resp.readInteger()
+	arr_len, _, err := r.readInteger()
 	if err != nil {
 		return value, err
 	}
@@ -112,7 +110,7 @@ func (resp *RESP) readArray() (Value, error) {
 
 	for i := 0; i < arr_len; i++ {
 		// recursion happens here [read the next stream of bytes.]
-		val, err := resp.Read() 
+		val, err := r.Read() 
 		if err != nil {
 			return value, err
 		}
@@ -123,83 +121,23 @@ func (resp *RESP) readArray() (Value, error) {
 	return value, nil
 }
 
-func (resp *RESP) readBulk() (Value, error) {
+func (r *Resp) readBulk() (Value, error) {
 	val := Value{}
 	val.typ = "bulk"
 
 	// read next byte to know the length of the string
 	// indicates the length of the string 
-	size, _, err := resp.readInteger()
+	size, _, err := r.readInteger()
 	if err != nil {
 		return val, err
 	}
 
 	bulk := make([]byte, size)
-	resp.reader.Read(bulk)
+	r.reader.Read(bulk)
 	val.bulk = string(bulk)
 
 	//read trailing line [CLRF]
-	resp.readLine()
+	r.readLine()
 
 	return val, nil
 }
-
-func main() {
-	input := "*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n"
-	resp := NewRESP(strings.NewReader(input))
-
-	val, err := resp.Read()
-	if err != nil {
-		fmt.Println("Error: ", err.Error())
-		os.Exit(1)
-	}
-
-	fmt.Println("Value: ", val)
-
-}
-
-
-// // this is a simple RESP parser.
-// func main() {
-// 	input := "$5\r\nahmed\r\n"
-// 	ParseResp(input)
-// }
-
-// func ParseResp(input string) string {
-// 	// given a string like this '$5/r/n/ahmed/r/n'
-// 	// we need to parse this, this string according to RESP
-// 	// the example given is a bulk string starts
-// 	// with $ and the length of the value (5))
-
-// 	reader := bufio.NewReader(strings.NewReader(input))
-
-// 	// read first byte from the buffer
-// 	firstCharBuf, _ := reader.ReadByte()
-
-// 	// now if the first byte read is not '$' then we reject it
-// 	// why? becuase we are first considering a bulk string.
-// 	if firstCharBuf != '$' {
-// 		fmt.Println("Error: Invalid type provided. Only bulk strings are supported")
-// 		os.Exit(1)
-// 	}
-
-// 	// indicates the length of the string 
-// 	size, _ := reader.ReadByte()
-
-// 	inputSize, _ := strconv.ParseInt(string(size), 10, 64)
-
-// 	// current position in parsing: ['$', '5', '/r', '/n', 'ahmed', '/r', '/n']
-// 	//																			    ^
-// 	reader.ReadByte()
-
-// 	// current position in parsing: ['$', '5', '/r', '/n', 'ahmed', '/r', '/n']
-// 	//																						     ^
-// 	reader.ReadByte()
-
-
-// 	name := make([]byte, inputSize)
-// 	reader.Read(name)
-
-// 	fmt.Println(string(name))
-// 	return string(name)
-// }
