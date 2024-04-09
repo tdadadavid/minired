2cb68f125d5389598a21d595b02859b0f489e6c8
tdadadavid <davidtofunmidada@gmail.com>
2024-04-09 21:17:08 +0100

    add units tests for 'readArray' function

--- lib/resp_test.go
+++ lib/resp_test.go
@@ -8,6 +8,35 @@ import (
 )
 
 
+func TestReadArray_WhenCorrectSyntazIsNotPassed(t *testing.T) {
+	t.Run("It returns error when wrong RESP array syntax is sent", func(t *testing.T) {
+		wrong_arr_string := "*wrong\r\n$5\r\nbayom"
+		reader := strings.NewReader(wrong_arr_string)
+		resp := NewResp(reader)
+		_, err := resp.readArray()
+
+		assert.Error(t, err)
+		assert.ErrorContains(t, err, "invalid syntax")
+	})
+}
+
+func TestReadArray_WhenCorrectSyntazIsPassed(t *testing.T) {
+	t.Run("The contents of the array is the  same as the RESP from the client", func(t *testing.T) {
+		// ignore the RESP array identicator [*] because READ already handles that.
+		resp_arr := "2\r\n$1\r\nA\r\n$1\r\nB\r\n"
+        reader := strings.NewReader(resp_arr)
+		resp := NewResp(reader)
+		result, err := resp.readArray()
+
+		assert.Nil(t, err)
+
+		assert.Len(t, result.array, 2)
+		assert.Equal(t, result.array[0].bulk, "A")
+		assert.Equal(t, result.array[1].bulk, "B")
+	})
+}
+
+
 
 func TestReadBulk_WhenCorrectSyntacIsNotPassed(t *testing.T) {
 	t.Run("It fails when a wrong bulk string syntax is sent from client.", func(t *testing.T) {
